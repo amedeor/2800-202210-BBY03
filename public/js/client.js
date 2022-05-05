3769668b3821d4e57d060c39204b0925e0c8f1b9
Amedeo Romano <amedeoromano@outlook.com>
2022-05-05 01:33:06 -0700

    Added input validation for fields in the login and sign up pages, changed the design of the login form and sign up form, fixed page redirection when going back to the login page from sign up page

--- public/js/client.js
+++ public/js/client.js
@@ -10,30 +10,34 @@ const backButton = document.querySelector("#back-button");
 
 if (loginButton != null) {
   loginButton.addEventListener("click", async e => {
-    e.preventDefault();
 
     let usernameElement = document.querySelector("#username");
     let passwordElement = document.querySelector("#password");
 
-    let username = usernameElement.value;
-    let password = passwordElement.value;
+    if (usernameElement.checkValidity() !== false && passwordElement.checkValidity() !== false) {
 
-    let response = await fetch("/login", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body: `username=${username}&password=${password}`
-    });
+      e.preventDefault();
 
-    let parsedResponse = await response.json();
-
-    if (parsedResponse.status === "fail") {
-      document.querySelector("#error-message").innerHTML = "";
-      document.querySelector("#error-message").insertAdjacentText("afterbegin", parsedResponse.message);
-    }
-    else {
-      window.location.replace("/profile");
+      let username = usernameElement.value;
+      let password = passwordElement.value;
+  
+      let response = await fetch("/login", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `username=${username}&password=${password}`
+      });
+  
+      let parsedResponse = await response.json();
+  
+      if (parsedResponse.status === "fail") {
+        document.querySelector("#error-message").innerHTML = "";
+        document.querySelector("#error-message").insertAdjacentText("afterbegin", parsedResponse.message);
+      }
+      else {
+        window.location.replace("/profile");
+      }
     }
   });
 }
@@ -47,7 +51,6 @@ if (goToSignupButton != null) {
 
 if (signupButton != null) {
   signupButton.addEventListener("click", async e => {
-    e.preventDefault();
 
     let signupFNameElement = document.querySelector("#signup-fName");
     let signupLNameElement = document.querySelector("#signup-lName");
@@ -55,26 +58,34 @@ if (signupButton != null) {
     let signupUsernameElement = document.querySelector("#signup-username");
     let signupPasswordElement = document.querySelector("#signup-password");
 
-    let signupFName = signupFNameElement.value;
-    let signupLName = signupLNameElement.value;
-    let signupEmail = signupEmailElement.value;
-    let signupUsername = signupUsernameElement.value;
-    let signupPassword = signupPasswordElement.value;
-    
-
-    let response = await fetch("/createUser", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body: `signupFName=${signupFName}&signupLName=${signupLName}&signupEmail=${signupEmail}&signupUsername=${signupUsername}&signupPassword=${signupPassword}`
-    });
-
-    let parsedResponse = await response.json();
-
-    if (parsedResponse.status === "fail") {
-      document.querySelector("#error-message").innerHTML = "";
-      document.querySelector("#error-message").insertAdjacentText("afterbegin", parsedResponse.message);
+    //Check the validity of each HTML5 element
+    //If the HTML5 element validity doesn't pass, then do not fetch /createUser and prevent user from signing up
+    if (signupFNameElement.checkValidity() !== false && signupLNameElement.checkValidity() !== false && signupEmailElement.checkValidity() !== false &&
+    signupUsernameElement.checkValidity() != false && signupPasswordElement.checkValidity() != false) {
+      
+      e.preventDefault();
+      
+      let signupFName = signupFNameElement.value;
+      let signupLName = signupLNameElement.value;
+      let signupEmail = signupEmailElement.value;
+      let signupUsername = signupUsernameElement.value;
+      let signupPassword = signupPasswordElement.value;
+      
+  
+      let response = await fetch("/createUser", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `signupFName=${signupFName}&signupLName=${signupLName}&signupEmail=${signupEmail}&signupUsername=${signupUsername}&signupPassword=${signupPassword}`
+      });
+  
+      let parsedResponse = await response.json();
+  
+      if (parsedResponse.status === "fail") {
+        document.querySelector("#error-message").innerHTML = "";
+        document.querySelector("#error-message").insertAdjacentText("afterbegin", parsedResponse.message);
+      }
     }
     else {
       // window.location.replace("/profile");
@@ -85,6 +96,7 @@ if (signupButton != null) {
 if (backButton != null) {
   backButton.addEventListener("click", async e => {
     e.preventDefault();
-    window.location.replace("/");
+    window.location.replace("/login");
+    console.log("back button clicked");
   });
 }
