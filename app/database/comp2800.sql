02c6a0e67eb263f59ffa16cec45869434e860293
Amedeo Romano <amedeoromano@outlook.com>
2022-05-05 19:16:13 -0700

    Fixed the formatting of the SQL queries

--- app/database/comp2800.sql
+++ app/database/comp2800.sql
@@ -15,12 +15,12 @@ CREATE TABLE IF NOT EXISTS bby03_user(
 
 INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (1, 'bgreen', 'Bill', 'Green', 'bgreen@email.com', 'password123', 'admin', '/img/avatar1.svg');
 
-INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (2, 'swhite', 'Sally', 'White', 'swith@email.com', 'password123', 'admin', '/img/avatar2.svg' );
+INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (2, 'swhite', 'Sally', 'White', 'swith@email.com', 'password123', 'admin', '/img/avatar2.svg');
 
-INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (3, 'bsmith', 'Bob', 'Smith', 'bsmith@email.com', 'password123', 'regular', '/img/avatar3.svg' );
+INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (3, 'bsmith', 'Bob', 'Smith', 'bsmith@email.com', 'password123', 'regular', '/img/avatar3.svg');
 
-INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (4, 'tbrown', 'Tim', 'Brown', 'tbrown@email.com', 'password123', 'regular', '/img/avatar4.svg' );
+INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (4, 'tbrown', 'Tim', 'Brown', 'tbrown@email.com', 'password123', 'regular', '/img/avatar4.svg');
 
-INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (5, 'eblack', 'Emily', 'Black', 'eblack@email.com', 'password123', 'regular', '/img/avatar5.svg' );
+INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (5, 'eblack', 'Emily', 'Black', 'eblack@email.com', 'password123', 'regular', '/img/avatar5.svg');
 
-INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (6, 'jwilson', 'John', 'Wilson', 'jwilson@email.com', 'password123', 'regular', '/img/avatar6.svg' );
+INSERT INTO bby03_user (user_id, user_username, user_firstname, user_lastname, user_email, user_password, user_type, user_avatar_url) VALUES (6, 'jwilson', 'John', 'Wilson', 'jwilson@email.com', 'password123', 'regular', '/img/avatar6.svg');
