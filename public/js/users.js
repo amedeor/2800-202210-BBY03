c45ef035a0e93942e815a8b5a9034f7d2495e14c
Patrick Cammayo <pcammayo03@gmail.com>
2022-05-12 11:40:13 -0700

    Added popup window for the users.html when 'edit' is clicked. Fixed undefined error when the admin doesn't upload an image for avatar change on the edit form.

--- public/js/users.js
+++ public/js/users.js
@@ -19,7 +19,7 @@ async function getUsers() {
 
       // str += `<tr><td class='id'>${row.user_id}</td><td class='username'><span>${row.user_username}</span></td><td class='firstname'><span>${row.user_firstname}</span></td><td class='lastname'><span>${row.user_lastname}</span></td><td class='email'><span>${row.user_email}</span></td><td class='password'><span>${row.user_password}</span></td><td class='usertype'><span>${row.user_type}</span></td><td class='useravatarurl'><span>${row.user_avatar_url}</span></td><td class='editbutton'><span><button>Edit</button></span></td><td class='deleteButton'><span><button>Delete</button></span></td></tr>`
 
-      str += `<tr><td class='id'>${row.user_id}</td><td class='username'>${row.user_username}</td><td class='firstname'>${row.user_firstname}</td><td class='lastname'>${row.user_lastname}</td><td class='email'>${row.user_email}</td><td class='password'>${row.user_password}</td><td class='usertype'>${row.user_type}</td><td class='useravatarurl'>${row.user_avatar_url}</td><td ><button class='editButton'>Edit</button></sp</td><td><button class='deleteButton'>Delete</button></td></tr>`
+      str += `<tr><td class='id'>${row.user_id}</td><td class='username'>${row.user_username}</td><td class='firstname'>${row.user_firstname}</td><td class='lastname'>${row.user_lastname}</td><td class='email'>${row.user_email}</td><td class='password'>${row.user_password}</td><td class='usertype'>${row.user_type}</td><td id="user${row.user_id}avatarURL" class='useravatarurl'>${row.user_avatar_url}</td><td ><button class='editButton'>Edit</button></sp</td><td><button class='deleteButton'>Delete</button></td></tr>`
 
     }
 
@@ -50,6 +50,10 @@ async function getUsers() {
 
 
 function editRow(e) {
+
+  let modalWindow = document.querySelector(".update-form-window");
+
+  modalWindow.style.display = "block";
 
   console.log("editRow");
 
@@ -87,19 +91,27 @@ let submitButton = document.querySelector("#submit");
 submitButton.addEventListener("click", async e => {
   e.preventDefault();
 
+
+
   let form = document.querySelector("#update-record-form");
   let fileUploadInput = document.querySelector("#image-upload");
 
   let id = document.querySelector("#id").value;
+  let oldAvatarURL = document.querySelector(`#user${id}avatarURL`).innerText;
+
   let username = document.querySelector("#username").value;
   let firstname = document.querySelector("#firstname").value;
   let lastname = document.querySelector("#lastname").value;
   let email = document.querySelector("#email").value;
   let password = document.querySelector("#password").value;
   let usertype = document.querySelector("#usertype").value;
+  
+  console.log(`This is the avatar url for user #${id}: ${oldAvatarURL}`);
 
   if (fileUploadInput.value != "") {
     userAvatarUrl = document.querySelector("#image-upload").value;
+  } else {
+    userAvatarUrl = oldAvatarURL;
   }
 
   let response = await fetch("/update-user-id", {
@@ -128,6 +140,11 @@ submitButton.addEventListener("click", async e => {
 
   //reset the user data form
   form.reset();
+
+  let modalWindow = document.querySelector(".update-form-window");
+
+  modalWindow.style.display = "none";
+  
 })
 
 getUsers();
@@ -201,6 +218,8 @@ async function uploadImage(e, username) {
   getUsers();
 
 }
+
+
 
 
 
