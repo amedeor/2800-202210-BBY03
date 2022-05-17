cf5686fb4c20924f6ab556c266fd7a8826d9c952
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2022-05-17 11:36:33 -0700

    everything about creating and adding a post now works, except adding pictures, and the database isn't setup yet.

--- public/js/postdeal.js
+++ public/js/postdeal.js
@@ -1,8 +1,8 @@
-let dealSubmitButton = document.querySelector("#deal-submit-button");
+let dealSubmitButton = document.querySelector("#create-deal-submit-button");
 
 dealSubmitButton.addEventListener("click", e => { 
   e.preventDefault();
-
+  createPost();
 
 }
 );
@@ -15,13 +15,12 @@ async function createPost() {
   let createForm = document.querySelector("#create-record-form");
   let createFileUploadInput = document.querySelector("#create-image-upload");
 
-  let id = document.querySelector("#id").value;
 
   let createPostname = document.querySelector("#createdealname").value;
-  let createFirstname = document.querySelector("#createdealprice").value;
-  let createLastname = document.querySelector("#createdealdescription").value;
-  let createEmail = document.querySelector("#createdealexpirydate").value;
-  let createPassword = document.querySelector("#createdealphotos").value;
+  let createDealPrice = document.querySelector("#createdealprice").value;
+  let createDealDesc = document.querySelector("#createdealdescription").value;
+  let createDealExpireDate = document.querySelector("#createdealexpirydate").value;
+  // let createDealPhotos = document.querySelector("#createdealphotos").value;
 
   // if (createFileUploadInput.value != "") {
   //   createPostAvatarUrl = document.querySelector("#create-image-upload").value;
@@ -34,7 +33,7 @@ async function createPost() {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
     },
-    body: `createPostname=${createPostname}&createPassword=${createPassword}&createFirstname=${createFirstname}&createLastname=${createLastname}&createEmail=${createEmail}&createPosttype=${createPosttype}&createPostAvatarUrl=${createPostAvatarUrl}`
+    body: `createPostname=${createPostname}&createDealPrice=${createDealPrice}&createDealDesc=${createDealDesc}&createDealExpireDate=${createDealExpireDate}`
   });
 
   let parsedResponse = await response.json();
@@ -45,14 +44,5 @@ async function createPost() {
   } else {
     console.log(parsedResponse.status);
   }
-
-
-  uploadCreateImage(createPostname);
-
-  //refresh the table after updating the record.
-  getUsers();
-
-  // //reset the user data form
-   createForm.reset();
 
 }
