21caad6474b04127c05155b3082bcc7f1f17e07a
Shayan_Zahedanaraki <shayan.zahedana@gmail.com>
2022-05-19 16:08:34 -0700

    quick bug fix for edit deal labels still being red after clicking closing and openning edit deal again

--- public/js/profile.js
+++ public/js/profile.js
@@ -538,6 +538,16 @@ $("#update-deal-container").data("dealID", dealID).dialog({
         //select this dialog and close it when cancel is pressed
         $("#update-deal-form").trigger("reset"); //clear the form when the cancel button is clicked
         $(this).dialog("close");
+        let dealexpirelabel = document.getElementById("updatedealexpirelabel");
+        dealexpirelabel.classList.remove("error");
+        let dealdescriptionlabel = document.getElementById("updatedealdescriptionlabel");
+        dealdescriptionlabel.classList.remove("error");
+        let deallocationlabel = document.getElementById("updatedeallocationlabel");
+        deallocationlabel.classList.remove("error");
+        let dealpricelabel = document.getElementById("updatedealpricelabel");
+        dealpricelabel.classList.remove("error");
+        let dealnamelabel = document.getElementById("updatedealnamelabel");
+        dealnamelabel.classList.remove("error");
       },
     }
   ],
