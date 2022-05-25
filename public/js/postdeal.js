4f3494d5b30bfddbfaa6b9305c5fbf3636064676
Patrick Cammayo <pcammayo03@gmail.com>
2022-05-25 10:08:39 -0700

    Reorganized css files to avoid overlap.

--- public/js/postdeal.js
+++ public/js/postdeal.js
@@ -1,8 +1,5 @@
 "use strict";
-
 let postDealLink = document.querySelector("#post-deal-link");
-
-
 postDealLink.addEventListener("click", e => {
   let dealexpirelabel = document.getElementById("dealexpirelabel");
   dealexpirelabel.classList.remove("error");
@@ -16,39 +13,30 @@ postDealLink.addEventListener("click", e => {
   dealnamelabel.classList.remove("error");
   $("#post-deal-container").dialog("open");
 })
-
 //Function to upload a new avatar image on the user's profile page
 async function postDeal() {
-
   const imageUploadElement = document.querySelector('#dealphotos');
-
   const formData = new FormData();
-
   //Use a loop to get the image from the image upload input and store it in a variable called file
   for (let i = 0; i < imageUploadElement.files.length; i++) {
     formData.append("files", imageUploadElement.files[i]);
   }
-
   let dealName = document.querySelector("#dealname");
   let dealPrice = document.querySelector("#dealprice");
-  let dealDescription = document.querySelector("#dealdescription");
+  // let dealDescription = document.querySelector("#dealdescription");
   let dealLocation = document.querySelector("#deallocation");
   let dealExpiryDate = document.querySelector("#dealexpirydate");
-
-  if (dealName.checkValidity() !== false && dealPrice.checkValidity() !== false && dealDescription.checkValidity() !== false &&
+  if (dealName.checkValidity() !== false && dealPrice.checkValidity() !== false && tinymce.activeEditor.getContent() !== "" &&
     dealLocation.checkValidity() != false && dealExpiryDate.checkValidity() != false) {
-
     formData.append("dealName", dealName.value);
     formData.append("dealPrice", dealPrice.value);
-    formData.append("dealDescription", dealDescription.value);
+    formData.append("dealDescription", tinymce.activeEditor.getContent());
     formData.append("dealLocation", dealLocation.value);
     formData.append("dealExpiryDate", dealExpiryDate.value);
-
     const options = {
       method: 'POST',
       body: formData,
     };
-
     await fetch("/post-deal", options
     ).then(function (res) {
       getDeals();
@@ -56,7 +44,6 @@ async function postDeal() {
     );
   }
 }
-
 $("#post-deal-container").dialog({
   modal: true,
   fuild: true, //prevent horizontal scroll bars on mobile layout
@@ -75,7 +62,7 @@ $("#post-deal-container").dialog({
         if (document.querySelector("#dealname").value != "") {
           if (document.querySelector("#dealprice").value != "") {
             if (document.querySelector("#deallocation").value != "") {
-              if (document.querySelector("#dealdescription").value != "") {
+              if (tinymce.activeEditor.getContent() != "") {
                 if (document.querySelector("#dealexpirydate").value != "") {
                   postDeal();
                   $("#deal-form").trigger("reset"); //clear the form when the cancel button is clicked
@@ -135,10 +122,12 @@ $("#post-deal-container").dialog({
     getDeals();
   }
 });
-
 //This block of code to center the jQuery UI modal popup when the window is resized is from
 //https://stackoverflow.com/questions/3060146/how-to-auto-center-jquery-ui-dialog-when-resizing-browser
 //with adaptatations made to apply to my modal window
 $(window).resize(function () {
   $("#post-deal-container").dialog("option", "position", { my: "center", at: "center", of: window });
+});
+tinymce.init({
+  selector: "#dealdescription"
 });
